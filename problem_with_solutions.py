0ef44a2f062c1c71234ec8370193fe5c2f225cd9
Sanjoy paul <sanjoypaulagt95@gmail.com>
2024-11-02 14:31:21 +0530

    Update problem_with_solutions.py

--- problem_with_solutions.py
+++ problem_with_solutions.py
@@ -17,5 +17,19 @@ def twoSum(nums: List[int], target: int) -> List[int]:
 
 ''' 1. 2SUM problem solution with o(n) approach
        nums = [2,7,11,15]
-       target = 9 say as example
+       target = 9 say as example using left and right pointer
+       
 '''
+def twoSum(self, numbers: List[int], target: int) -> List[int]:
+       n = len(numbers)
+       l = 0
+       r = n - 1
+       
+       while l < r:
+              summ = numbers[l] + numbers[r]
+       if summ == target:
+              return [l + 1, r + 1]
+       elif summ < target:
+              l += 1
+       else:
+              r -= 1
